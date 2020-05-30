74cead5ff2ac1d4f11e414062fa0ee2290b35353
nairitb <38316739+nairitb@users.noreply.github.com>
2020-05-30 21:29:37 +0530

    Update arduino.ino

--- Program/arduino/arduino.ino
+++ Program/arduino/arduino.ino
@@ -6,8 +6,8 @@
 *
 */
 // define pins numbers
-const int trigPin = 9;
-const int echoPin = 8;
+const int trig = 9;
+const int echo = 8;
 const int Valve = 11;
 
 // define variables
@@ -15,30 +15,30 @@ long duration;
 int distance;
 void setup() {
   
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trig, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echo, INPUT); // Sets the echoPin as an Input
   pinMode(Valve, OUTPUT); // Configure the pin connected to the Valve as OUTPUT
-  Serial.begin(9600); // Starts the serial communication
+  
 }
 
 void loop() {
   // Clear the trigPin
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trig, LOW);
   delayMicroseconds(2);
   
   // Set the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trig, LOW);
   
   // Read the echoPin, return the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echo, HIGH);
   
   // Calculate the distance
   distance= duration*0.034/2;
 
   //Condition for opening Water Valve
-  if(distance>10 && distance<50){
+  if(distance>10 && distance<60){
     digitalWrite(Valve, LOW); //Turns on the solenoid water valve
   }
   
@@ -46,7 +46,4 @@ void loop() {
     digitalWrite(Valve, HIGH); //Turns off the solenoid water valve
    }
   
-  // Print the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
 }
